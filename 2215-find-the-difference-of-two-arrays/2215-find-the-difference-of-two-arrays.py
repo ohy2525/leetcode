ea1d22e4243e2d795861561eadaa9728b1cf53b0
ohy2525 <81752763+ohy2525@users.noreply.github.com>
2024-06-15 16:58:18 +0900

    Time: 137 ms (42.75%), Space: 12.4 MB (17.1%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -1,14 +1,21 @@
 class Solution(object):
     def findDifference(self, nums1, nums2):
         answer = [[], []]
-        set1 = set(nums1)
-        set2 = set(nums2)
+        hashTable = {}
         for n in nums1:
-            if not n in set2 and not n in answer[0]:
-                answer[0].append(n)
-        
+            if n not in hashTable:
+                hashTable[n] = [True, False]
+
         for n in nums2:
-            if not n in set1 and not n in answer[1]:
-                answer[1].append(n)
+            if n in hashTable:
+                hashTable[n][1] = True
+            else:
+                hashTable[n] = [False, True]
+
+        for k, v in hashTable.items():
+            if v[0] and not v[1]:
+                answer[0].append(k)
+            elif not v[0] and v[1]:
+                answer[1].append(k)
 
         return answer
