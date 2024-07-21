36e0ff754a7f9a0263f161089bb7b477f93d6a17
ohy2525 <81752763+ohy2525@users.noreply.github.com>
2024-07-21 15:46:04 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1, r = 0;
+        for (int p : piles) r = Math.max(r, p);
+
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (cond(piles, m, h)) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return l;
+    }
+
+    private boolean cond(int[] piles, int k, int h) {
+        int curCnt = 0;
+        for (int p : piles) {
+            curCnt += p / k + (p % k ? 1 : 0);
+            if (curCnt > h) return false;
+        }
+        return true;
+    }
+}
