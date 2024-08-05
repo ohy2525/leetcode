1ce0949e122597fdd656b7f27a0ac32933fbf3da
ohy2525 <81752763+ohy2525@users.noreply.github.com>
2024-08-05 23:09:41 +0900

    Time: 0 ms (100%), Space: 45.9 MB (39.76%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null){
+            return null;
+        }
+        if(root.val==key){
+            return help(root);
+        }
+        TreeNode ans=root;
+        while(root !=null){
+            if(root.val>key){
+                    if(root.left!=null && root.left.val==key){
+                        root.left= help(root.left);
+                        break;
+                    }else{
+                        root=root.left;
+                    }
+            }else{
+                    if(root.right!=null && root.right.val==key){
+                        root.right= help(root.right);
+                        break;
+                    }else{
+                        root=root.right;
+                    }
+                }
+            }
+            return ans;
+    }
+    public TreeNode help(TreeNode root){
+        if(root.left==null){
+            return root.right;
+        }else if(root.right==null){
+            return root.left;
+        }else{
+            TreeNode rightone=root.right;
+            TreeNode lastright=find(root.left);
+            lastright.right=rightone;
+            return root.left;
+        }
+       // return null;
+    }
+    public TreeNode find(TreeNode root){
+        if(root.right==null){
+            return root;
+        }
+        return find(root.right);
+    }
+}
