5e9a9a4d6595bb7b33b02c55a0960c3f5c0597d8
ohy2525 <81752763+ohy2525@users.noreply.github.com>
2024-08-27 23:41:09 +0900

    test

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,13 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int idx = 1;
+        int j = 1;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
-                nums[idx] = nums[i];
-                idx++;
+                nums[j] = nums[i];
+                j++;
             }
         }
 
-        return idx;
+        return j;
     }
 }
