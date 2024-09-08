574c737ab8ac4c6426f2f3756443273e1bca8fc9
ohy2525 <81752763+ohy2525@users.noreply.github.com>
2024-09-08 23:12:51 +0900

    Time: 1 ms (98.93%), Space: 44.6 MB (79.78%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] hash = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            hash[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (hash[c - 'a'] > 0) {
+                hash[c - 'a']--;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
